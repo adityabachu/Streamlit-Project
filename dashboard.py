06776fc5cd4cce5df754590ef4bf3be9c07f091a
Aditya Bachu <99969931+adityabachu@users.noreply.github.com>
2023-06-18 12:14:36 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -12,6 +12,7 @@ st.dataframe(data.head())
 st.header("Descriptive statistics of the data :")
 st.dataframe(data.describe())
 
+st.header("Let's look at the amount of usage of apps :")
 
 figure = px.bar(data_frame=data, 
                 x = "Date", 
@@ -21,13 +22,23 @@ figure = px.bar(data_frame=data,
 
 st.plotly_chart(figure)
 
-
+st.header("Now let’s have a look at the number of notifications from the apps :")
 
 figure = px.bar(data_frame=data, 
                 x = "Date", 
                 y = "Notifications", 
                 color="App", 
                 title="Notifications")
+
+st.plotly_chart(figure)
+
+st.header("Now let’s have a look at the number of times the apps opened :")
+
+figure = px.bar(data_frame=data, 
+                x = "Date", 
+                y = "Times opened", 
+                color="App",
+                title="Times Opened")
 
 st.plotly_chart(figure)
 
